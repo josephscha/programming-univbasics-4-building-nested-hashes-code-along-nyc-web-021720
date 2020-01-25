813715b2a88fd455eb1093732ec1f2dc1e96aa3d
Joseph S. Cha <josephshincha@gmail.com>
2020-01-25 02:56:18 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,15 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => [
-        {
-        :name => "Benvolio",
-        :age => "17"
-   },
-   {
-        :name => "Mercutio",
-        :age => "18"
-   }
+      :hero_friends => [ {name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
